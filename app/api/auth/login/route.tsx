d0836d3b728c1bda1393f39fefe5cedebab4c59e
Yeider Adrian Mina <yeideradrian.noba@gmail.com>
2024-11-28 12:48:33 -0500

    Api login y logout

--- app/api/auth/login/route.tsx
+++ app/api/auth/login/route.tsx
@@ -4,9 +4,9 @@ import { loginWithRest } from "@/lib/useAuth";
 export async function POST(request: Request) {
     try {
         const { identifier, password } = await request.json();
-        const data = await loginWithRest(identifier, password);
-        const { jwt, user } = data;
-        const response = NextResponse.json({ message: "Login successful", user: user });
+        const dataResponse = await loginWithRest(identifier, password);
+        const { jwt, data } = dataResponse;
+        const response = NextResponse.json({ message: "Login successful", user: data });
         response.cookies.set("jwt", jwt, {path: "/", httpOnly: true, secure: process.env.NODE_ENV === "production",});
 
         return response;
