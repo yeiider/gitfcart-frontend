3041760ee3ba1d223f1e987f862b8ffeb9984aa5
Yeider Adrian Mina <yeideradrian.noba@gmail.com>
2024-11-29 01:15:56 -0500

    Actualizacion de usuarios

--- app/interfaces/userInterface.tsx
+++ app/interfaces/userInterface.tsx
@@ -1,15 +1,15 @@
 export interface User {
-    id: number;
-    documentId: string;
-    username: string;
-    email: string;
-    provider: string;
-    confirmed: boolean;
-    blocked: boolean;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-    name: string;
-    lastName: string;
-    rut: string;
+    id?: number;
+    documentId?: string;
+    username?: string;
+    email?: string;
+    provider?: string;
+    confirmed?: boolean;
+    blocked?: boolean;
+    createdAt?: string;
+    updatedAt?: string;
+    publishedAt?: string;
+    name?: string;
+    lastName?: string;
+    rut?: string;
 }
